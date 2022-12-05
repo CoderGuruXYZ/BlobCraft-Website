73e66f74b6757625b06ced8a70625f92df9ff49c
CoderGuruXYZ <72657147+CoderGuruXYZ@users.noreply.github.com>
2022-12-05 14:05:21 +0000

    console dot log plan x

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,12 +2,14 @@ var nav = document.querySelector("nav");
 var navTop = document.querySelector(".navTop");
 
 function loadAll() {
+    console.log("func started");
     $("nav").html("");
 
     var names = ["Home", "Discord", "Client"];
     var links = ["index.html", "discord.html", "client.html"];
 
     for(i = 0; i < names.length; i++) {
+        console.log("Loop started");
         var link = document.createElement("div");
         link.classList.add("link");
         
