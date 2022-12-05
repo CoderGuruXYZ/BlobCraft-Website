e76ffe04d1c4898aa19c52586847cd8f50db8631
CoderGuruXYZ <singhlionuk@gmail.com>
2022-12-05 13:55:32 +0000

    Navbar Frame

---
+++ scripts/navbar.js
@@ -0,0 +1,23 @@
+var nav = document.querySelector("nav");
+var navTop = document.querySelector("navTop");
+
+function loadAll() {
+    $("nav").html("");
+
+    var names = ["Home", "Discord", "Client"];
+    var links = ["index.html", "discord.html", "client.html"];
+
+    for(i = 0; i < names.length; i++) {
+        var link = document.createElement("div");
+        link.classList.add("link");
+        
+        var linkA = document.createElement("a");
+        linkA.classList.add("linkA");
+        linkA.innerHTML = names[i];
+        linkA.href = links[i];
+
+        link.appendChild(linkA);
+
+        navTop.appendChild(link);
+    }
+}
