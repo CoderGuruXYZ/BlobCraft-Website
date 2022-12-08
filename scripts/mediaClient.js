4c740f22cc9d0d1d9a7a42b1a73b68edfd3634e4
CoderGuruXYZ <singhlionuk@gmail.com>
2022-12-08 13:53:51 +0000

    Mobile Friendly

---
+++ scripts/mediaClient.js
@@ -0,0 +1,7 @@
+if(innerWidth < 960) {
+    $(".infoBox").css("margin-top", "140px");
+    $(".infoBox").css("margin-bottom", "-140px");
+
+    $("footer").css("position", "absolute");
+    $("footer").css("bottom", "0");
+}
---
+++ scripts/mediaIPS.js
@@ -0,0 +1,5 @@
+if(innerWidth < 960) {
+    $(".infoBox").css("margin-top", "140px");
+    $(".infoBox").css("width", "100%");
+    $(".infoBox").css("max-width", "100%");
+}
